e3da0b1beeccf6c2187dc403d6a8cf7ca867b175
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-11 15:40:05 +0000

    incomplete MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {makeHappy, makeUnhappy} from './index';
+import {connect} from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = ({amIHappy, makeHappy, makeUnhappy}) => {
   const state = {
     additionalPrice: 0,
     car: {
@@ -34,15 +35,26 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
+        {/* <Header car={state.car} />
         <AddedFeatures car={state.car} />
       </div>
       <div className="box">
         <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <Total car={state.car} additionalPrice={state.additionalPrice} /> */}
+        
+        <p>
+          {
+            amIHappy ? ':)' : ':('
+          }
+        </p>
+        <button onClick={makeHappy}>happy</button>
+        <button onClick={makeUnhappy}>unhappy</button>
       </div>
     </div>
   );
 };
 
-export default App;
+export default connect(
+  state => state,
+  {makeHappy, makeUnhappy},
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {combineReducers, createStore} from 'redux';
+import {Provider } from 'react-redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const MAKE_HAPPY = 'MAKE_HAPPY';
+const MAKE_UNHAPPY = 'MAKE_UNHAPPY';
+
+function happyReducer(state = true, action) {
+    switch (action.type) {
+        case MAKE_HAPPY:
+            return true;
+
+            case MAKE_UNHAPPY:
+                return false;
+                default:
+                    return state;
+
+    }
+}
+
+const reducer = combineReducers({
+    amIHappy: happyReducer,
+})
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>,  document.getElementById('root'));
+
+
+export function makeHappy(){
+    return{
+        type: MAKE_HAPPY,
+
+    }
+}
+
+export function makeUnhappy(){
+    return {
+        type: MAKE_UNHAPPY,
+    }
+}
